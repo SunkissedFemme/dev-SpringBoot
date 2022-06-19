1212dc873ec5b754c022bccdbd86766d70d0f3d6
SunkissedFemme <craciun_bnc@yahoo.com>
2022-06-19 11:42:20 +0300

    Attempt to implement security

---
+++ demo/src/main/java/security/APIKeyAuthFilter.java
@@ -0,0 +1,23 @@
+package security;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class APIKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private String principalRequestHeader;
+
+    public APIKeyAuthFilter(String principalRequestHeader) {
+        this.principalRequestHeader = principalRequestHeader;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return request.getHeader(principalRequestHeader);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "N/A";
+    }
+
+}
